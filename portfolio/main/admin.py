77597d1e54af1419036a7c3deb17d49e92ef6ff5
feirahisham <feirahisham@gmail.com>
2025-06-14 08:44:00 +0800

    adding admin site for Profile, ProfileSection, ProfileField

--- portfolio/main/admin.py
+++ portfolio/main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Profile, ProfileSection, ProfileField
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(ProfileSection)
+admin.site.register(ProfileField)
